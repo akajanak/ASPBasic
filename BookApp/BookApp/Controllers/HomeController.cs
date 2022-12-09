7ac0d75dbb82fd6fcf75eb5988de85c5f5274261
akajanak <107241263+akajanak@users.noreply.github.com>
2022-12-09 13:41:53 +0545

    commiting all

---
+++ BookApp/BookApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "WEbGentlE";
+        }
+    }
+}
--- BookApp/BookApp/Program.cs
+++ BookApp/BookApp/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+/*
+ * Middleware Concept -- similar to nodejs 
+ * app.Use(async (Context, next) =>
+{
+    await Context.Response.WriteAsync("hello from first middleware");
+    next();
+});*/
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
